9602ea768a21f6ee041cf27a8c481e0a069ed21e
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-10 10:33:20 -0300

    Get game by id

--- src/main/java/com/thiagomatos/dslist/services/GameService.java
+++ src/main/java/com/thiagomatos/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.thiagomatos.dslist.dto.GameDTO;
 import com.thiagomatos.dslist.dto.GameMinDTO;
 import com.thiagomatos.dslist.entities.Game;
 import com.thiagomatos.dslist.repositories.GameRepository;
+
+
 
 @Service
 public class GameService {
@@ -15,9 +19,18 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
+	
 }
