31fb33602b964b03219c89bbe92fcedd94ee9100
Thiago Fratucci de Matos <thi_fm@yahoo.com.br>
2023-05-10 22:05:49 -0300

    SQL, projection, get games by list

--- src/main/java/com/thiagomatos/dslist/dto/GameMinDTO.java
+++ src/main/java/com/thiagomatos/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.thiagomatos.dslist.dto;
 
 import com.thiagomatos.dslist.entities.Game;
+import com.thiagomatos.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -14,12 +15,19 @@ public class GameMinDTO {
 	}
 
 	public GameMinDTO(Game entity) {
-		super();
 		this.id = entity.getId();
 		this.title = entity.getTitle();
 		this.year = entity.getYear();
 		this.imgUrl = entity.getImgUrl();
 		this.shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		this.id = projection.getId();
+		this.title = projection.getTitle();
+		this.year = projection.getYear();
+		this.imgUrl = projection.getImgUrl();
+		this.shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/thiagomatos/dslist/services/GameService.java
+++ src/main/java/com/thiagomatos/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.thiagomatos.dslist.dto.GameDTO;
 import com.thiagomatos.dslist.dto.GameMinDTO;
 import com.thiagomatos.dslist.entities.Game;
+import com.thiagomatos.dslist.projections.GameMinProjection;
 import com.thiagomatos.dslist.repositories.GameRepository;
 
 
@@ -32,5 +33,14 @@ public class GameService {
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	
 	
 }
